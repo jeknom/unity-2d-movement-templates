255ac82dbd18a8e8e567249860b8b7c8ea2573f0
Johannes Palvanen <palvanenj@gmail.com>
2021-01-14 20:20:40 +0200

    Add grid based movement template

---
+++ Assets/MovementTemplates/Grid/Scripts/Movable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Movable : MonoBehaviour
+{
+    [SerializeField] LayerMask blockingLayer;
+    [SerializeField, Range(0.01f, 1f)] float movementSpeed = 0.04f;
+
+    Vector3 targetPosition = Vector3.zero;
+
+    void Start()
+    {
+        this.targetPosition = this.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        this.Move();
+    }
+
+    protected void SetTargetPosition(Vector3 direction)
+    {
+        var boxCast = Physics2D.Raycast(this.transform.position, direction, 1f, this.blockingLayer);
+
+        if (boxCast || this.transform.position != this.targetPosition)
+        {
+            return;
+        }
+
+        this.targetPosition = this.transform.position + direction;
+    }
+
+    void Move()
+    {
+        if (this.transform.position != this.targetPosition)
+        {
+            this.transform.position =
+                Vector3.MoveTowards(this.transform.position, this.targetPosition, this.movementSpeed);
+        }
+    }
+}
---
+++ Assets/MovementTemplates/Grid/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Player : Movable
+{
+    void Update()
+    {
+        var moveDirection = this.GetMoveDirection();
+
+        if (moveDirection != Vector3.zero)
+        {
+            this.SetTargetPosition(moveDirection);
+        }
+    }
+
+    Vector3 GetMoveDirection()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            return Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            return Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            return Vector3.up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            return Vector3.down;
+        }
+
+        return Vector3.zero;
+    }
+}
